140ed436e278b3635c8ea43ffb93014bb65182f4
lior Shine <shinelior@gmail.com>
2019-12-13 20:10:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,9 @@ def square_array(array)
   counter=0 
   new_array=[]
   while counter<array.length   
-      arrgament_sq= array[counter]*array[counter]
-       new_array<<
+       arrgament_sq= array[counter]*array[counter]
+       new_array<< arrgament_sq  
+       counter+=1   
+     end
+     return new_array
 end
